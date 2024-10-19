83800cf1f0d9d52f2812fefb83ad55cf86c99756
AkemMensah <akemmensah@gmail.com>
2024-10-19 01:21:24 +0000

    updated a vercel url

--- e-commerce_api/app.js
+++ e-commerce_api/app.js
@@ -133,7 +133,7 @@ const swaggerOptions = {
             servers: [
                 {
                     url: process.env.NODE_ENV === 'production' 
-            ? 'https://https://e-commerce-node-amgvhqs6z-akemmensahs-projects.vercel.app/'  //Vercel deployment URL (production)
+            ? 'https://e-commerce-node-app.vercel.app/'  //Vercel deployment URL (production)
             : 'http://localhost:3000', // Localhost (development)
                 },
             ],
